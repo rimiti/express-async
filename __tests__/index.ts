b9be587390c43374b1326bca39d5c28b2571ca81
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-02-02 16:16:23 +0100

    chore(__tests__/index.ts) tests created

---
+++ __tests__/index.ts
@@ -0,0 +1,84 @@
+import {NextFunction, Request, Response} from "express";
+import {wrapAsync} from "../src";
+
+describe("wrapAsync", () => {
+  it("Should catch exceptions of a function passed into it.", async () => {
+    const error = new Error('Catch me.');
+    const fn = wrapAsync(() => {
+      throw error
+    });
+    expect(fn).toThrow(error);
+  });
+
+  it("Should call next with the error when an async function passed into it throws.", async () => {
+    const error = new Error('Catch me.');
+    const nextFn = jest.fn();
+    // @ts-ignore
+    const fn = wrapAsync(async (req: Request, res: Response, next: NextFunction) => {
+      throw error;
+    });
+
+    await fn(undefined, undefined, nextFn); // tslint:disable-line:no-unsafe-any
+    expect(nextFn).toHaveBeenCalledWith(error);
+  });
+
+  it("Should call next with the arguments when an async function passed into it calls next.", async () => {
+    const nextFn = jest.fn();
+    // @ts-ignore
+    const fn = wrapAsync(async (req: Request, res: Response, next: NextFunction) => {
+      next("Custom content.");
+    });
+
+    await fn(undefined, undefined, nextFn); // tslint:disable-line:no-unsafe-any
+    expect(nextFn).toHaveBeenCalledWith("Custom content.");
+  });
+
+  it("Should accept a non-async function.", async () => {
+    const nextFn = jest.fn();
+    // @ts-ignore
+    const fn = wrapAsync((req: Request, res: Response, next: NextFunction) => {
+      next("Custom content.");
+    });
+
+    await fn(undefined, undefined, nextFn); // tslint:disable-line:no-unsafe-any
+    expect(nextFn).toHaveBeenCalledWith("Custom content.");
+  });
+
+  it("Should accept a non-async function erroring.", async () => {
+    const error = new Error('Catch me.');
+    const nextFn = jest.fn();
+    // @ts-ignore
+    const fn = wrapAsync((req: Request, res: Response, next: NextFunction) => {
+      next(error)
+    });
+
+    await fn(undefined, undefined, nextFn); // tslint:disable-line:no-unsafe-any
+    expect(nextFn).toHaveBeenCalledWith(error);
+  });
+
+  it("Should handle thenables.", async () => {
+    const error = new Error('Catch me.');
+    let thenable: {
+      then(): void;
+    };
+    let triggerFailure: (error) => void;
+    const registeringThenable = new Promise((resolve: () => void) => {
+      thenable = {
+        // @ts-ignore
+        then: jest.fn((success: () => void, fail: () => void) => {
+          triggerFailure = fail;
+          resolve();
+        })
+      }
+    });
+
+    const next = jest.fn();
+    const catchingThenable = wrapAsync(() => thenable)(undefined, undefined, next); // tslint:disable-line:no-unsafe-any
+    await registeringThenable;
+    expect(thenable.then).toHaveBeenCalled();
+    expect(next).not.toHaveBeenCalled();
+    triggerFailure(error);
+    await catchingThenable;
+    expect(next).toHaveBeenCalledWith(error);
+  });
+});
